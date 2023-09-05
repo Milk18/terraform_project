ea41ea33d1d25d303a97df97e3f81fde724b0361
Ori <milk49@walla.co.il>
2023-09-05 14:43:42 +0300

    moved the data directory command to start in the db script

--- shell_scripts/db_script.bash
+++ shell_scripts/db_script.bash
@@ -2,10 +2,10 @@
 apt install nano
 apt install postgresql postgresql-contrib -y
 systemctl start postgresql.service
+sudo -u postgres psql -c data_directory = "/data1"
 sudo -u postgres psql -c "CREATE DATABASE flask_db;"
 sudo -u postgres psql -c "CREATE USER oriu WITH PASSWORD 'oriu';"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE flask_db TO oriu;"
 echo "listen_addresses = '*'" | sudo tee -a /etc/postgresql/*/main/postgresql.conf
 echo "host   all    all 10.1.0.0/24     md5" | sudo tee -a /etc/postgresql/*/main/pg_hba.conf
-sudo -u postgres psql -c data_directory = "/data1"
 sudo service postgresql restart
